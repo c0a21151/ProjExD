2369bb264d83a0578c55b6828c889c6709eab052
C0A21151 <c0a2115176@edu.teu.ac.jp>
2022-11-29 14:57:34 +0900

    練習

--- ex02/calc_T.py
+++ ex02/calc_T.py
@@ -1,31 +1,31 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-#練習3
+
+# 練習３
 def button_click(event):
     btn = event.widget
     num = btn["text"]
     if num == "=":
-        siki = entry.get() #数式の文字列
-        res = eval(siki) #数式文字列の評価
-        entry.delete(0, tk.END) #表示文字列の削除
-        entry.insert(tk.END, res) #結果の挿入
-        
-    else: #「=」以外の文字
-    #tkm.showinfo("", f"[{num}]ボタンがクリックされました")
-    #練習6
+        siki = entry.get() # 数式の文字列
+        res = eval(siki) # 数式文字列の評価
+        entry.delete(0, tk.END) # 表示文字列の削除
+        entry.insert(tk.END, res) # 結果の挿入
+    else: # 「=」以外のボタン字
+        #tkm.showinfo("", f"{num}ボタンがクリックされました")
+        # 練習６
         entry.insert(tk.END, num)
 
-#練習1
+    
+# 練習１
 root = tk.Tk()
-root.title("電卓基本機能")
 root.geometry("300x500")
 
-#練習4
-entry = tk.Entry(root, justify="right", wight=10, font=("", 40))
+# 練習４
+entry = tk.Entry(root, justify="right", width=10, font=("",40))
 entry.grid(row=0, column=0, columnspan=3)
 
-#練習2
+# 練習２
 r, c = 1, 0
 for num in range(9, -1, -1):
     button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
@@ -34,9 +34,9 @@ for num in range(9, -1, -1):
     c += 1
     if c%3 == 0:
         r += 1
-        c =0
+        c = 0
 
-#練習5
+# 練習５
 operators = ["+", "="]
 for ope in operators:
     button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("", 30))
@@ -45,6 +45,6 @@ for ope in operators:
     c += 1
     if c%3 == 0:
         r += 1
-        c =0
+        c = 0
 
 root.mainloop()
