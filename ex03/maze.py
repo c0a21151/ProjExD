97705f80573e450dfc839ed8778255eaa4dd5413
C0A21151 <c0a2115176@edu.teu.ac.jp>
2022-12-06 17:43:30 +0900

    修正 #5 #6

--- ex03/maze.py
+++ ex03/maze.py
@@ -19,22 +19,22 @@ def main_proc():
     if key == "Down": my += 1
     if key == "Left": mx -= 1
     if key == "Right": mx += 1
-    if maze_lst[mx][my] == 0: 
-        tori = tk.PhotoImage(file="9.png")
-        canvas.create_image(cx, cy, image=tori, tag="kokaton")
+    if maze_lst[mx][my] == 0: #移動先が道だったら
+        tori1 = tk.PhotoImage(file="9.png")
+        canvas.create_image(cx, cy, image=tori1, tag="kokaton9")
 
     if maze_lst[mx][my] == 1:  #移動先が壁だったら
         if key == "Up": my += 1
         if key == "Down": my -= 1
         if key == "Left": mx += 1
         if key == "Right": mx -= 1
-        tori = tk.PhotoImage(file="8.png")
-        canvas.create_image(cx, cy, image=tori, tag="kokaton")
+        tori2 = tk.PhotoImage(file="8.png")
+        canvas.create_image(cx, cy, image=tori2, tag="kokaton8")
 
     cx, cy = mx*100+50, my*100+50
     canvas.coords("kokaton", cx, cy)
 
-    if cx == ex and cy == ey:
+    if cx == ex and cy == ey: #こうかとんがりんごについたら
         label = tk.Label(root, text="おいしい!!", font=("", 100), fg="red")
         label.pack()
 
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
 
-    label0 = tk.Label(root, text="こうかとんはりんごが食べたい", font=("", 20), fg="black")
+    label0 = tk.Label(root, text="こうかとんはりんごが食べたい", font=("", 20), fg="black")  #こうかとんをりんごへ誘導
     label0.pack()
 
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
@@ -57,11 +57,11 @@ if __name__ == "__main__":
 
     mx, my = 1, 1
     cx, cy = mx*100+50, my*100+50
-    tori = tk.PhotoImage(file="0.png")
-    canvas.create_image(cx, cy, image=tori, tag="kokaton")
+    tori0 = tk.PhotoImage(file="0.png")
+    canvas.create_image(cx, cy, image=tori0, tag="kokaton0")  #こうかとんの初期画像
     ex, ey = mx*1500-150, my*900-150
-    get = tk.PhotoImage(file="apple.PNG")
-    canvas.create_image(ex, ey, image=get, tag="kokaton")
+    get = tk.PhotoImage(file="apple.PNG")  #ゴールはりんごの画像
+    canvas.create_image(ex, ey, image=get, tag="apple")
 
 
     key = ""
